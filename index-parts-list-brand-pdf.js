0311057b93387a1a9ffa428313517b1a08987da2
sewing-parts <sima77sewing@online.ua>
2022-08-26 14:24:15 +0300

    26082022a

---
+++ index-parts-list-brand-pdf.js
@@ -0,0 +1,41 @@
+const bonisPDF = [
+    { bonistitle: 'Bonis_3000_series_Parts_List', bonishref: 'https://drive.google.com/file/d/1zQRolSN7CJ1A2V4P_SfHDVopjnlx3eQS/view?usp=sharing' },
+    { bonistitle: 'Bonis_3012_3032_BD_Parts_List', bonishref: 'https://drive.google.com/file/d/1zLLS9xVhBqH0f7E-Lv9flPqp-dMnA8YC/view?usp=sharing' },
+    { bonistitle: 'Bonis_3014_3010_Parts_List', bonishref: 'https://drive.google.com/file/d/1zQRolSN7CJ1A2V4P_SfHDVopjnlx3eQS/view?usp=sharing' },
+    { bonistitle: 'Bonis_3032_3027_Parts_List', bonishref: 'https://drive.google.com/file/d/1zLLS9xVhBqH0f7E-Lv9flPqp-dMnA8YC/view?usp=sharing' },
+    { bonistitle: 'Bonis_3051_3064_Parts_List', bonishref: 'https://drive.google.com/file/d/1zQRolSN7CJ1A2V4P_SfHDVopjnlx3eQS/view?usp=sharing' },
+    { bonistitle: 'Bonis_3065_3066_Parts_List', bonishref: 'https://drive.google.com/file/d/1zLLS9xVhBqH0f7E-Lv9flPqp-dMnA8YC/view?usp=sharing' },
+    { bonistitle: 'Bonis_3067_3068_Parts_List', bonishref: 'https://drive.google.com/file/d/1zQRolSN7CJ1A2V4P_SfHDVopjnlx3eQS/view?usp=sharing' },
+    { bonistitle: 'Bonis_A21_A18_A15_Parts_List', bonishref: 'https://drive.google.com/file/d/1zLLS9xVhBqH0f7E-Lv9flPqp-dMnA8YC/view?usp=sharing' },
+    { bonistitle: 'Bonis_A21_A18_A16_Parts_List', bonishref: 'https://drive.google.com/file/d/1zQRolSN7CJ1A2V4P_SfHDVopjnlx3eQS/view?usp=sharing' },
+    { bonistitle: 'Bonis_B-27_B-32_Parts_List', bonishref: 'https://drive.google.com/file/d/1zLLS9xVhBqH0f7E-Lv9flPqp-dMnA8YC/view?usp=sharing' },
+    { bonistitle: 'Bonis_BG-12_BG-14_Parts_List', bonishref: 'https://drive.google.com/file/d/1zQRolSN7CJ1A2V4P_SfHDVopjnlx3eQS/view?usp=sharing' },
+    { bonistitle: 'Bonis_BH-12_BD-32_Parts_List', bonishref: 'https://drive.google.com/file/d/1zLLS9xVhBqH0f7E-Lv9flPqp-dMnA8YC/view?usp=sharing' },
+    { bonistitle: 'Bonis_BK-10_BK-12_Parts_List', bonishref: 'https://drive.google.com/file/d/1zQRolSN7CJ1A2V4P_SfHDVopjnlx3eQS/view?usp=sharing' },
+    { bonistitle: 'Bonis_BN-10_BC-14_Parts_List', bonishref: 'https://drive.google.com/file/d/1zLLS9xVhBqH0f7E-Lv9flPqp-dMnA8YC/view?usp=sharing' },
+    { bonistitle: 'Bonis_BT-11_T-12_Parts_List', bonishref: 'https://drive.google.com/file/d/1zQRolSN7CJ1A2V4P_SfHDVopjnlx3eQS/view?usp=sharing' },
+    { bonistitle: 'Bonis_SL-64_Parts_List', bonishref: 'https://drive.google.com/file/d/1zLLS9xVhBqH0f7E-Lv9flPqp-dMnA8YC/view?usp=sharing' },
+    { bonistitle: 'Bonis_SL-65_SB-66_Parts_List', bonishref: 'https://drive.google.com/file/d/1zQRolSN7CJ1A2V4P_SfHDVopjnlx3eQS/view?usp=sharing' },
+    { bonistitle: 'Bonis_T-57_Parts_List', bonishref: 'https://drive.google.com/file/d/1zLLS9xVhBqH0f7E-Lv9flPqp-dMnA8YC/view?usp=sharing' }
+]
+
+
+
+const barudanPDF = [
+    { bonistitle: 'Barudan_EliteBevt_Pro_Z9-15_01CB_Parts_List', bonishref: 'https://drive.google.com/file/d/1VPIpCxw2P9Oo2J4CT0ImL1bF31iDSpVb/view?usp=sharing' },
+    { bonistitle: 'Barudan_Elite_pro_Bead_ZQ_501_Parts_List', bonishref: 'https://drive.google.com/file/d/1xZtn235aSNJFeaELVyFh3yt-N-LTzGEi/view?usp=sharing' },
+    { bonistitle: 'Barudan_Elite_JR_Z1501CA_Parts_List', bonishref: 'https://drive.google.com/file/d/1qA0O-JcXYi1Od_WHlx9Z0HpfInslvosO/view?usp=sharing' },
+    { bonistitle: 'Barudan_Elite_Jr_Z3_z9_Parts_List', bonishref: 'https://drive.google.com/file/d/16lK-d-oH0oIE70LQxpFNXe38y6H6FgOG/view?usp=sharing' },
+    { bonistitle: 'Barudan_Elite_Jr_Bead_ZN_01_Parts_List', bonishref: 'https://drive.google.com/file/d/15I3U9mPqiqfL-SnurDXLgBCCeCfDkyul/view?usp=sharing' },
+    { bonistitle: 'Barudan_Bevt_Z1501CB_II_Parts_List', bonishref: 'https://drive.google.com/file/d/18tBiappUEwjKvbImrZApLTTqh_jgVqsL/view?usp=sharing' },
+    { bonistitle: 'Barudan_Bevt_Z1201C_Z1501C_Parts_List', bonishref: 'https://drive.google.com/file/d/1qv8-Clu1_mN8-vm6tK9ye1QOk5Lq6X-O/view?usp=sharing' },
+    { bonistitle: 'Barudan_Bevt_C1201C_Z1501CII_Parts_List', bonishref: 'https://drive.google.com/file/d/1JfT8Q4u30uabfcz81IRPXrgth-CVWZdM/view?usp=sharing' },
+    { bonistitle: 'Barudan_Bent_ZN_ZQ_Parts_List', bonishref: 'https://drive.google.com/file/d/1hhCx-5Z648WTUJ0gDxTv9wKl9y2h0W8l/view?usp=sharing' },
+    { bonistitle: 'Barudan_Bent_ZN_ZQ_201_Parts_List', bonishref: 'https://drive.google.com/file/d/1DSRM0OCvcNPITwluTf8-TW-eunQOMNnS/view?usp=sharing' },
+    { bonistitle: 'Barudan_Bent_ZN_101U_Parts_List', bonishref: 'https://drive.google.com/file/d/1Dm8hAFU29MZ9WMQz5b2CVXJ5UiJR5lZn/view?usp=sharing' },
+    { bonistitle: 'Barudan_Bedthe_ZQ_C01_Parts_List', bonishref: 'https://drive.google.com/file/d/1skWY1NrW14IpV2SgW5mQXY5VPaFPP7dF/view?usp=sharing' },
+    { bonistitle: 'Barudan_Beat_IV_YS_T_Parts_List', bonishref: 'https://drive.google.com/file/d/18nKypO3RmL15ULSN2U7B-aeKMkMWuvd7/view?usp=sharing' },
+    { bonistitle: 'Barudan_Beat_IV_Supplement_Parts_List', bonishref: 'https://drive.google.com/file/d/1vKCgA6tL5Jmk6synHt7wybTlLoF6g-PQ/view?usp=sharing' },
+    { bonistitle: 'Barudan_Beat_IV_Parts_List', bonishref: 'https://drive.google.com/file/d/1w_rs7qrDur2NnAbGPQkBYNEXBcOxVHk1/view?usp=sharing' },
+]
+export { bonisPDF, barudanPDF };
