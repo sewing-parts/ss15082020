7f741c616ccb8db68cc4e376b78ee7d319a61a5a
sewing-parts <sima77sewing@online.ua>
2022-11-21 14:28:42 +0200

    21112022ff

--- send_mail.php
+++ send_mail.php
@@ -1,17 +1,6 @@
 <?php
 
-// Выключить отчеты об ошибках
-error_reporting(0);
-// Добавлять в отчет все PHP ошибки
-error_reporting(E_ALL);
-// Аналогично error_reporting(E_ALL);
-ini_set('error_reporting', E_ALL);
-// Показывать в отчете часть ошибок ошибок
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-// Аналогично предыдущему, но будут показываться еще ошибки связанные с перенеменными
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-// Будут показываться все ошибки кроме E_NOTICE
-error_reporting(E_ALL & ~E_NOTICE);
+
 
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
