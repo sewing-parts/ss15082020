fc0debcc592403511d2c276f77e87f3bdfab5b85
sewing-parts <sima77sewing@online.ua>
2022-11-23 11:56:56 +0200

    23112022a-0033

--- app.js
+++ app.js
@@ -93,11 +93,11 @@ fileInput.addEventListener("change", () => {
         let reader = new FileReader();
         let listItem = document.createElement("li");
         let fileName = i.name;
-        let fileSize = (i.size / 1024).toFixed(9);
+        let fileSize = (i.size / 1024).toFixed(1);
         listItem.innerHTML = `<p>${fileName}</p><p>${fileSize}KB</p>`;
 
         if (fileSize >= 1024) {
-            fileSize(fileSize / 1024).toFixed(9)
+            fileSize(fileSize / 1024).toFixed(1)
             listItem.innerHTML = `<p>${fileName}</p><p>${fileSize}MB</p>`;
         }
         fileList.appendChild(listItem);
